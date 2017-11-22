1b7f1ee202632cf969d5aceb5be1d243da3eaddd
mentlsve <sven.mentl@gmail.com>
2017-11-22 20:52:57 +0100

    switching to gae flexible quickstart code

--- api.go
+++ api.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+        "fmt"
+        "log"
+        "net/http"
 )
 
-func init() {
-	r := gin.New()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	http.Handle("/",r)
+func main() {
+        http.HandleFunc("/", handle)
+        http.HandleFunc("/_ah/health", healthCheckHandler)
+        log.Print("Listening on port 8080")
+        log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+                http.NotFound(w, r)
+                return
+        }
+        fmt.Fprint(w, "Hello world!")
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "ok")
 }
